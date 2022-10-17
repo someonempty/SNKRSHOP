28fa98af6e021e8f8d25352c20588553f14ba11d
someonempty <someonempty@gmail.com>
2022-10-18 00:19:48 +0300

    Верстка поля корзины и промокода

---
+++ src/ts/catalog.ts
@@ -0,0 +1,5 @@
+export const Catalog = () => {
+    const mainCatalog = document.createElement('main');
+    mainCatalog.classList.add('catalog');
+}
+
---
+++ src/ts/main.ts
@@ -0,0 +1,4 @@
+import '../style.css';
+import { Catalog } from './catalog';
+
+Catalog();
