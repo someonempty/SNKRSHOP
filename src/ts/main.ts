efa877969d22bd9c8027211d7318e2821a1e0923
someonempty <someonempty@gmail.com>
2022-11-09 15:35:56 +0300

    commit

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -7,6 +7,8 @@ import { Promo } from './promo';
 
 const main:any = document.querySelector('.catalog');
 const basketlist:any = document.querySelector('.basket-list');
+let promoWrapper:any = document.querySelector('.promo-wrapper');
+let totalWrapper:any = document.querySelector('.total-wrapper');
 
 
 
@@ -15,14 +17,6 @@ type Sneaker = {id: number, name: string, price: string, src: string};
 const sneakers:Sneaker[] = [{id: 1, name: 'New Balance 574 Vintage Brights', price: '$ 650', src: 'src/img/vintage.png'}, {id: 2, name: 'New Balance Made in UK 920 Chinese New Year', price: '$ 1 200', src: 'src/img/chinese.png'}, {id: 3, name: 'New Balance 373 Modern Classics', price: '$ 800', src: 'src/img/classic.png'}, {id: 4, name: 'New Balance Made in UK 670 Chinese New Year', price: '$ 780', src: 'src/img/chinese-brown.png'}, {id: 5, name: 'New Balance X-Racer Utility', price: '$ 1 000', src: 'src/img/racer.png'}, {id: 6, name: 'New Balance 5740 Think Colorfully', price: '$ 940', src: 'src/img/colorfully.png'}];
 
 let basketElements:Sneaker[] = [{id: 1, name: 'New Balance 574 Vintage<br>Brights', price: '650', src: 'src/img/vintage.png'}, {id: 2, name: 'New Balance Made in UK 920 Chinese New Year', price: '1 200', src: 'src/img/chinese.png'}];
-
-// main.appendChild(sneakers.forEach(element => {
-// return (Card(element.name, element.price, element.src, () => addBasketItem(element.id, element.price, element.price, element.src )))
-// }))
-
-// sneakers.forEach(element => {
-//     main.appendChild(Card(element.name, element.price, element.src, () => addBasketItem(element.id, element.price, element.price, element.src )))
-// })
 
 const catalogRender = () => { 
     sneakers.forEach(sneaker => {
@@ -37,16 +31,24 @@ const basketRender = () => {
      )})
 }
 
+const promoRender = () => {
+    promoWrapper.appendChild(Promo());
+    promoWrapper = '';
+}
+
+const totalRender = () => {
+    basketElements.forEach(element => {
+       totalWrapper.appendChild(Total(element.price))
+    })
+}
+
 const render = () => {
     // aside.innerHTML = '';
     main.innerHTML = '';
     catalogRender();
     basketRender();
-    Promo(),
-    basketElements.forEach(element => {
-        Total(element.price)
-    })
-    
+    promoRender();
+    totalRender();
 }
 
 const addBasketItem = (id:any, name:any, price:any, src:any) => {
@@ -63,13 +65,9 @@ const deleteBasketItem = (id:any) => {
     basketElements = basketElements.filter(element => element.id !== id)
     basketlist.innerHTML = '';
     render();
-    console.log(basketElements);
 }
 
-
-console.log(basketElements);
 render();
-console.log(basketElements);
 
 // const wrapper:any = document.querySelector('.wrapper');
 // const main:any = document.createElement('main');
@@ -84,3 +82,11 @@ console.log(basketElements);
 
 // wrapper?.appendChild(main);
 // wrapper?.appendChild(aside);
+
+// main.appendChild(sneakers.forEach(element => {
+// return (Card(element.name, element.price, element.src, () => addBasketItem(element.id, element.price, element.price, element.src )))
+// }))
+
+// sneakers.forEach(element => {
+//     main.appendChild(Card(element.name, element.price, element.src, () => addBasketItem(element.id, element.price, element.price, element.src )))
+// })
